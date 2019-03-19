417a9a66d0a5d5f12dd340e4b8de9d51e88feba8
Javontay McElroy <dinomightnetwork@gmail.com>
2019-03-19 17:18:58 -0500

    Finsihed.

--- data/postsRouter.js
+++ data/postsRouter.js
@@ -2,7 +2,7 @@ const postsRouter = require('express').Router();
 
 const db = require('./db.js');
 
-//-------- New Post  --------//
+// --------------------------------- NEW POST -----------------------------------//
 postsRouter.post('/', (req, res) => {
   const newPost = res.body;
   if (newPost.title && newPost.contents) {
@@ -20,7 +20,7 @@ postsRouter.post('/', (req, res) => {
   }
 });
 
-// ------Get all Posts-------//
+// --------------------------------- GET POST -----------------------------------//
 postsRouter.get('/', (req, res) => {
   db.find()
     .then(posts => {
@@ -31,38 +31,52 @@ postsRouter.get('/', (req, res) => {
     });
 });
 
-// // --------------------------------- DELETE -----------------------------------//
-postsRouter.delete('/:id', async (req, res) => {
-  try {
-    const count = await post.remove(req.params.id);
-    if (count > 0) {
-      res.status(200).json({ message: 'The post cant sit with us!' });
-    } else {
-      res.status(404).json({ message: 'Looked, didnt find nothing, fam...' });
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      message: 'Tried to remove post, but it told me no...'
+// --------------------------------- POST -----------------------------------//
+
+postsRouter.get('/:id', (req, res) => {
+  db.findById(req.params.id)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      res.status(407).json({ message: "That user doesn't exist" });
     });
-  }
+});
+
+// --------------------------------- DELETE -----------------------------------//
+postsRouter.delete('/:id', (req, res) => {
+  db.remove(req.params.id)
+    .then(remove => {
+      res.status(204).end();
+    })
+    .catch(error => {
+      res.status(500).json('Homie beat me up, cant get it!');
+    });
 });
 
 // // --------------------------------- PUT -----------------------------------//
-postsRouter.put('/:id', async (req, res) => {
-  try {
-    const post = await post.update(req.params.id, req.body);
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({ message: 'I cant find post! Oh nooo!' });
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      message: 'Error Error! Cant update post!'
+postsRouter.put('/:id', (req, res) => {
+  const { title, contents } = req.body;
+
+  db.findById(req.params.id)
+    .then(post => {
+      if (post) {
+        if (title && contents) {
+          db.update(req.params.id, req.body).then(post => {
+            res.status(200).json(req.body);
+          });
+        } else {
+          res.status(400).json({
+            message: 'Please provide a post'
+          });
+        }
+      } else {
+        res.status(404).json({ message: "That post doesn't exist" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to update post' });
     });
-  }
 });
 
 module.exports = postsRouter;
